5161a68fce2509d4f25da72a90f3ffd53aa29410
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-01 01:24:24 -0500

    Actualizar owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('✅️ *Chat Baneado Con Exito, Ya No Podran Usar SakuraBotLite - MD*');
+  m.reply('✅️ *Chat Baneado Con Exito, Ya No Podran Usar 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿*');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
