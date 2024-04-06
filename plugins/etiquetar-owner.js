9b7346fdae78b258d07d082a897bc1d96edcb154
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-06 00:44:35 -0500

    Actualizar etiquetar-owner.js

--- plugins/etiquetar-owner.js
+++ plugins/etiquetar-owner.js
@@ -1,9 +1,9 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*🌩 ️𝗡𝗼 𝗘𝘁𝗶𝗾𝘂𝗲𝘁𝗲𝘀 𝗔 𝗠𝗶 𝗖𝗿𝗲𝗮𝗱𝗼𝗿, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯', body: '𝖭𝗈 𝖤𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌 𝖠 𝖬𝗂 𝖢𝗋𝖾𝖺𝖽𝗈𝗋', sourceUrl: 'https://github.com/diegojadibot/SakuraBotLite-MD'}}})
+conn.reply(m.chat, `*🌩 ️𝗡𝗼 𝗘𝘁𝗶𝗾𝘂𝗲𝘁𝗲𝘀 𝗔 𝗠𝗶 𝗖𝗿𝗲𝗮𝗱𝗼𝗿, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '🤖🥊𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿🤖🥊', body: '𝖭𝗈 𝖤𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌 𝖠 𝖬𝗂 𝖢𝗋𝖾𝖺𝖽𝗈𝗋', sourceUrl: 'https://github.com/ale-rmz/YerayBot-MD'}}})
 }
-handler.customPrefix = /@573012482597/i;
+handler.customPrefix = /@593992402778/i;
 handler.command = new RegExp();
 
 export default handler;
