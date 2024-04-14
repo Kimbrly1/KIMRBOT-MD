fcd49d6668686238a56d6ca69050219dc5795f02
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-13 21:07:26 -0500

    Update etiquetar-owner.js

--- plugins/etiquetar-owner.js
+++ plugins/etiquetar-owner.js
@@ -1,7 +1,7 @@
 let handler = async (m, {conn, text}) => {
   let name = await conn.getName(m.sender);
 
-conn.reply(m.chat, `*🌩 ️𝗡𝗼 𝗘𝘁𝗶𝗾𝘂𝗲𝘁𝗲𝘀 𝗔 𝗠𝗶 𝗖𝗿𝗲𝗮𝗱𝗼𝗿, si es algo urgente contacta con el a su chat privado para mas informacion*` ,m, { contextInfo:{ externalAdReply: {title: '🤖🥊𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿🤖🥊', body: '𝖭𝗈 𝖤𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌 𝖠 𝖬𝗂 𝖢𝗋𝖾𝖺𝖽𝗈𝗋', sourceUrl: 'https://github.com/ale-rmz/YerayBot-MD'}}})
+conn.reply(m.chat, `*⚠𝘼𝙡𝙩𝙤 𝙃𝙖𝙮 𝙋𝙚𝙧𝙧𝙤⚠ 𝙉𝙤 𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙚𝙨 𝘼 𝙈𝙞 𝘾𝙧𝙚𝙖𝙙𝙤𝙧🚫, 𝙎𝙞 𝙀𝙨 𝘼𝙡𝙜𝙤 𝙐𝙧𝙜𝙚𝙣𝙩𝙚 𝘾𝙤𝙣𝙩𝙖𝙘𝙩𝙖 𝘾𝙤𝙣 𝙚𝙡 𝘼 𝙎𝙪 𝘾𝙝𝙖𝙩 𝙋𝙧𝙞𝙫𝙖𝙙𝙤 𝙋𝙖𝙧𝙖 𝙈𝙖𝙨 𝙄𝙣𝙛𝙤𝙧𝙢𝙖𝙘𝙞ó𝙣😃*` ,m, { contextInfo:{ externalAdReply: {title: '𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿', body: '𝖭𝗈 𝖤𝗍𝗂𝗊𝗎𝖾𝗍𝖾𝗌 𝖠 𝖬𝗂 𝖢𝗋𝖾𝖺𝖽𝗈𝗋', sourceUrl: 'https://github.com/ale-rmz/YerayBot-MD'}}})
 }
 handler.customPrefix = /@593992402778/i;
 handler.command = new RegExp();
