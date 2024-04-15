de94c5eb79baf439492d5c2b8d5b62b3d3c9c000
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-15 10:29:13 -0500

    Actualizar gc-hidetag.js

--- plugins/gc-hidetag.js
+++ plugins/gc-hidetag.js
@@ -36,7 +36,7 @@ const handler = async (m, {conn, text, participants, isOwner, isAdmin}) => {
     }
   }
 };
-handler.command = /^(hidetag|notificar|notify)$/i;
+handler.command = /^(hidetag|notificar|notify|n)$/i;
 handler.group = true
 handler.admin = true
 handler.register = true
