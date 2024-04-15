aa425c0c603d0f8de39dd07be931c9691a2ee4d9
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-15 17:08:21 -0500

    Actualizar plugins

---
+++ plugins/ff-4vs4.js
@@ -0,0 +1,38 @@
+import fg from 'api-dylux' 
+import fetch from 'node-fetch'
+import { savefrom, facebookdl, facebookdlv2 } from '@bochilteam/scraper'
+import fbDownloader from 'fb-downloader-scrapper'
+import { facebook } from "@xct007/frieren-scraper"
+import axios from 'axios'
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+if (!args[0]) throw `
+╭──────⚔──────╮
+ㅤㅤㅤ  𝑽𝑬𝑹𝑺𝑼𝑺 
+╰──────⚔──────╯
+╭──────────────╮
+│ㅤ *𝑴𝑶𝑫𝑶*  : COMPE
+│ㅤ⏱ 𝑯𝑶𝑹𝑨𝑹𝑰𝑶 
+│ㅤ🇲🇽 𝑴𝑬𝑿 : 
+│ㅤ🇪🇨 𝑬𝑪:
+│➥ 𝑬𝑺𝑪𝑼𝑨𝑫𝑹𝑨: 
+│➥ 𝑱𝑼𝑮𝑨𝑫𝑶𝑹𝑬𝑺:
+│
+│     ☞𝑬𝑺𝑪𝑼𝑨𝑫𝑹𝑨: 
+│
+│👑 ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│⚜️ ➤ 
+│
+│ㅤ☞𝑺𝑼𝑷𝑳𝑬𝑵𝑻𝑬:
+│⚜️ ➤ 
+│⚜️ ➤ 
+╰─────────────╯
+                
+` 
+}
+handler.command = /^(vs4|4vs4)$/i
+handler.register = true
+handler.group = true
+handler.admin = true
+export default handler
