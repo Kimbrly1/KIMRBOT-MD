47eef3922698e87035fc8aba3f83d86cd6051793
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-20 08:10:47 -0500

    Actualizar _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -8,7 +8,7 @@ if (process.send) { process.send('uptime')
 _muptime = await new Promise(resolve => { process.once('message', resolve) 
 setTimeout(resolve, 2000) }) * 1000}
 let uptime = clockString(_uptime)
-let bio = `💥 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿┃𝗠𝗶 𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝙤 𝗘𝘀: ${uptime}\n𝗪𝗵𝗮𝘁𝘀𝗮𝗽𝗽 𝗕𝗼𝘁 👺`
+let bio = `⏳𝗠𝗶 𝗧𝗶𝗲𝗺𝗽𝗼 𝗔𝗰𝘁𝗶𝘃𝙤: ${uptime}\n𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿✨`
 await this.updateProfileStatus(bio).catch(_ => _)
 //await this.updateProfilePicture(imagen6.catch(_ => _)
 setting.status = new Date() * 1
