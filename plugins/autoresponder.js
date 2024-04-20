0a786f6246bc3d844a57b5d79c3394d78354d34e
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-20 09:58:39 -0500

    Actualizar autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen8, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿🥷🏻💥', body: '𝖡𝗒: Sebas - 𝖶𝖺𝗈𝗌', sourceUrl: `https://github.com/ale-rmz/YerayBot-MD`, thumbnail: imagen6}}})}
+contextInfo: { externalAdReply: { title: '𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿🥷🏻💥', body: '𝖡𝗒: Sebas - 𝖶𝖺𝗈𝗌', sourceUrl: `https://www.instagram.com/sebas.mvp_official0?igsh=cGk3Zmd0c3Bta280`, thumbnail: imagen6}}})}
 
 return !0 }
 export default handler
