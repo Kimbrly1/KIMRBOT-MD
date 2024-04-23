e2cbe4cf3f70a6f195ebf0f420e48bcd5b427013
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-22 19:15:39 -0500

    Actualizar _antitoxic.js

--- plugins/_antitoxic.js
+++ plugins/_antitoxic.js
@@ -14,12 +14,12 @@ export async function before(m, {isAdmin, isBotAdmin, isOwner}) {
 
   if (isToxic && chat.antiToxic && !isOwner && !isAdmin) {
     user.warn += 1;
-    if (!(user.warn >= 5)) await m.reply('*🍧 ' + `${user.warn == 1 ? `Hola @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, decir la palabra "${isToxic}" está prohibido en este grupo. Advertencia: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
+    if (!(user.warn >= 5)) await m.reply('*⚠️ ' + `${user.warn == 1 ? `𝑯𝑶𝑳𝑨 𝑴𝑰 𝑨𝑴𝑰𝑮@ @${m.sender.split`@`[0]}` : `@${m.sender.split`@`[0]}`}, 𝑫𝑬𝑪𝑰𝑹 𝑳𝑨 𝑷𝑨𝑳𝑨𝑩𝑹𝑨 "${isToxic}" 𝑬𝑺𝑻𝑨 𝑷𝑹𝑶𝑯𝑰𝑩𝑰𝑫𝑶 𝑬𝑵 𝑬𝑺𝑻𝑬 𝑮𝑹𝑼𝑷𝑶.: ${user.warn}/5.` + '*', false, {mentions: [m.sender]});
   }
 
   if (user.warn >= 5) {
     user.warn = 0;
-    await m.reply(`*🥀 Hola @${m.sender.split`@`[0]}, superaste las 5 advertencias por lo que serás eliminado de este grupo por tu comportamiento.*`, false, {mentions: [m.sender]});
+    await m.reply(`*🥀 𝑯𝑬𝒀 𝑷𝑬𝑹𝑹@ @${m.sender.split`@`[0]}, 𝑺𝑼𝑷𝑬𝑹𝑨𝑺𝑻𝑬𝑺 𝑳𝑨𝑺 5 𝑨𝑫𝑽𝑬𝑹𝑻𝑬𝑵𝑪𝑰𝑨𝑺 𝑨𝑯𝑶𝑹𝑨 𝑻𝑬 𝑽𝑨𝑺 𝑪𝑶𝑴𝑶 𝑼𝑵/𝑨 𝑷𝑬𝑹𝑹@ 𝑸𝑼𝑬 𝑬𝑹𝑬𝑺🤑⚡`, false, {mentions: [m.sender]});
     user.banned = true;
     await mconn.conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove');
     // await this.updateBlockStatus(m.sender, 'block')
