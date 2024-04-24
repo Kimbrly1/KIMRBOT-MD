6f7a609d1024a3454118015f47813a0baf357071
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-24 18:36:35 -0500

    Actualizar bot-saludo.js

--- plugins/bot-saludo.js
+++ plugins/bot-saludo.js
@@ -1,4 +1,4 @@
-//comando de https://github.com/ale-rmz/YerayBot-MD
+//comando de https://github.com/sebasmpv/YerayBot-MD
 
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
