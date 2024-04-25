6a56b919c6b7ef625a5dfe9aa7fa90fe6aa5598c
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-25 16:11:47 -0500

    Actualizar info-cuentasoficiales.js

--- plugins/info-cuentasoficiales.js
+++ plugins/info-cuentasoficiales.js
@@ -3,7 +3,7 @@
 let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    await conn.sendMessage(m.chat, { react: { text: '🧸', key: m.key } })
+    await conn.sendMessage(m.chat, { react: { text: '😍', key: m.key } })
 let str = `𝑩𝑰𝑬𝑵𝑽𝑬𝑵𝑰𝑫𝑶 𝑨 𝑳𝑨𝑺 𝑪𝑼𝑬𝑵𝑻𝑨𝑺 𝑶𝑭𝑰𝑪𝑰𝑨𝑳𝑬𝑺 🤩⚡
 ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 🤖 *𝐁𝐨𝐭 𝐎𝐟𝐢𝐜𝐢𝐚𝐥:*
