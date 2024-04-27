26593bc110e83e10761760fe9b596f3659c5bedf
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-04-27 13:08:03 -0500

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,17 +1,17 @@
 import {search, download} from 'aptoide-scraper';
 const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*🚩 Ingrese Un Nombre De Una Apk Para Descargarlo 🎌*`;
+ if (!text) throw `*⚡ 𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙐𝙣 𝙉𝙤𝙢𝙗𝙧𝙚 𝘿𝙚 𝙐𝙣𝙖 𝘼𝙥𝙠 𝙋𝙖𝙧𝙖 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧𝙡𝙤 🎌*`;
   try {    
     const searchA = await search(text);
     const data5 = await download(searchA[0].id);
-    let response = `🍭 _Descargador De Aptoide_ 📲\n\n🐱 *Nombre:* ${data5.name}\n🚩 *Package:* ${data5.package}\n🎌 *Ultima Actualización:* ${data5.lastup}\n💝 *Tamaño:* ${data5.size}`
+    let response = `🔍 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙙𝙤𝙧 𝘿𝙚 𝘼𝙥𝙩𝙤𝙞𝙙𝙚 📲\n\n😺 *𝙉𝙤𝙢𝙗𝙧𝙚:* ${data5.name}\n🚩 *𝙋𝙖𝙘𝙠𝙖𝙜𝙚:* ${data5.package}\n🎌 *𝙐𝙡𝙩𝙞𝙢𝙖 𝘼𝙘𝙩𝙪𝙖𝙡𝙞𝙯𝙖𝙘𝙞𝙤𝙣:* ${data5.lastup}\n💝 *𝙋𝙚𝙨𝙤:* ${data5.size}`
     await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
  if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*🚩 La App Es Muy Pesada No Se Pudo Enviar El Archivo 🎌.*'}, {quoted: m});
+      return await conn.sendMessage(m.chat, {text: '𝙇𝙖 𝘼𝙥𝙥 𝙀𝙨 𝙈𝙪𝙮 𝙋𝙚𝙨𝙖𝙙𝙖 𝙉𝙤 𝙎𝙚 𝙋𝙪𝙙𝙤 𝙀𝙣𝙫𝙞𝙖𝙧 𝙀𝙡 𝘼𝙧𝙘𝙝𝙞𝙫𝙤😨.*'}, {quoted: m});
     }
     await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
   } catch {
-    throw `*🍭 Error No Se Pudo Enviar El Archivo.*`;
+    throw `*⚠ 𝙀𝙧𝙧𝙤𝙧 𝙉𝙤 𝙎𝙚 𝙋𝙪𝙙𝙤 𝙀𝙣𝙫𝙞𝙖𝙧 𝙀𝙡 𝘼𝙧𝙘𝙝𝙞𝙫𝙤.*`;
   }    
 };
 handler.command = /^(apk|modapk|dapk2|aptoide|aptoidedl)$/i;
