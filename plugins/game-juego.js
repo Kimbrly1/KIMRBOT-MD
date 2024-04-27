f9c30757d6bf153615dd28b79a32e49ca68aee03
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-04-27 13:35:20 -0500

    Update game-juego.js

--- plugins/game-juego.js
+++ plugins/game-juego.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, text }) => {
-  let { key } = await conn.sendMessage(m.chat, { text: "Tas caliente! Ahora te hare una paja" }, { quoted: m });
+  let { key } = await conn.sendMessage(m.chat, { text: "𝙏𝙖𝙨 𝙘𝙖𝙡𝙞𝙚𝙣𝙩𝙚! 𝘼𝙝𝙤𝙧𝙖 𝙩𝙚 𝙝𝙖𝙧𝙚 𝙪𝙣𝙖 𝙥𝙖𝙟𝙖" }, { quoted: m });
   const array = [
     "8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D", "8==👊=D", "8==👊==D", "8=👊===D", "8👊====D", "8=👊===D","8==👊==D", "8===👊=D", "8====👊D","8==👊==D", "8===👊=D", "8=👊===D", "8=👊===D", "8==👊==D", "8===👊=D", "8====👊D💦"
   ];
@@ -8,7 +8,7 @@ let handler = async (m, { conn, usedPrefix, text }) => {
     await conn.sendMessage(m.chat, { text: `${item}`, edit: key }, { quoted: m });
     await new Promise(resolve => setTimeout(resolve, 20)); // Delay 5 seconds
   }
-  return conn.sendMessage(m.chat, { text: `Oh, se corrió en menos de 1 hora!`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
+  return conn.sendMessage(m.chat, { text: `𝙊𝙝, 𝙨𝙚 𝙘𝙤𝙧𝙧𝙞𝙤 𝙚𝙣 𝙢𝙚𝙣𝙤𝙨 𝙙𝙚 𝟭 𝙝𝙤𝙧𝙖!`.trim() , edit: key, mentions: [m.sender] }, { quoted: m });
 };
 
 handler.help = ['pajeame'];
