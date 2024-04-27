129f170f1a9e5db6370d02a14db4147e4f4b2aa9
sebasmpvofficial <166235155+sebasmpvofficial@users.noreply.github.com>
2024-04-27 13:44:33 -0500

    Update gc-unwarn.js

--- plugins/gc-unwarn.js
+++ plugins/gc-unwarn.js
@@ -5,12 +5,12 @@ const handler = async (m, {conn, text, command, usedPrefix}) => {
   else who = m.chat;
   const user = global.db.data.users[who];
   const bot = global.db.data.settings[conn.user.jid] || {};
-  const warntext = `*Etiqueta a una persona o responde a un mensaje del grupo*\n\n*Ejemplo:*\n*${usedPrefix + command} @${global.suittag}*`;
+  const warntext = `*𝙀𝙩𝙞𝙦𝙪𝙚𝙩𝙖 𝙖 𝙪𝙣𝙖 𝙥𝙚𝙧𝙨𝙤𝙣𝙖 𝙤 𝙧𝙚𝙨𝙥𝙤𝙣𝙙𝙚 𝙖 𝙪𝙣 𝙢𝙚𝙣𝙨𝙖𝙟𝙚 𝙙𝙚𝙡 𝙜𝙧𝙪𝙥𝙤*\n\n*Ejemplo:*\n*${usedPrefix + command} @${global.suittag}*`;
   if (!who) throw m.reply(warntext, m.chat, {mentions: conn.parseMention(warntext)});
   if (m.mentionedJid.includes(conn.user.jid)) return;
-  if (user.warn == 0) throw '*El usuario tiene 0 advertencia*';
+  if (user.warn == 0) throw '*𝙀𝙡 𝙪𝙨𝙪𝙖𝙧𝙞𝙤 𝙩𝙞𝙚𝙣𝙚 𝟬 𝙖𝙙𝙫𝙚𝙧𝙩𝙚𝙣𝙘𝙞𝙖*';
   user.warn -= 1;
-  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} Se le quitó una advertencia\n*ADVERTENCIAS ${user.warn}/3*`, null, {mentions: [who]});
+  await m.reply(`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `♻️ *@${who.split`@`[0]}*`} 𝙎𝙚 𝙡𝙚 𝙦𝙪𝙞𝙩𝙤 𝙪𝙣𝙖 𝙖𝙙𝙫𝙚𝙧𝙩𝙚𝙣𝙘𝙞𝙖\n*𝘼𝘿𝙑𝙀𝙍𝙏𝙀𝙉𝘾𝙄𝘼𝙎 ${user.warn}/3*`, null, {mentions: [who]});
 };
 handler.command = /^(unwarn|delwarn|quitaradvertencia|deladvertencia|delwarning)$/i;
 handler.group = true;
