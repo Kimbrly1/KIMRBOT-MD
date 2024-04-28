04c5124ec48d1bf4afccc3d2e8c00f631f9de39b
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-27 21:51:37 -0500

    Actualizar owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = false;
-  m.reply('✅️ *Chat Desbaneado Con Exito, Ya Pueden Usar YerayBot - MD*');
+  m.reply('✅️ 𝘾𝙝𝙖𝙩 𝘿𝙚𝙨𝙗𝙖𝙣𝙚𝙖𝙙𝙤 𝙔𝙖 𝙋𝙪𝙚𝙙𝙚𝙣 𝙐𝙨𝙖𝙧 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿');
 };
 handler.help = ['unbanchat'];
 handler.tags = ['owner'];
