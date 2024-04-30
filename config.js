8a9275c8fa551e63b20ba8f0b873ff9bf72622df
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-30 14:53:13 -0500

    Actualizar config.js

--- config.js
+++ config.js
@@ -91,11 +91,11 @@ global.mes = d.toLocaleDateString('es', {month: 'long'});
 global.año = d.toLocaleDateString('es', {year: 'numeric'});
 global.tiempo = d.toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true});
 //* ****************************
-global.wm2 = `${dia} ${fecha}\nYerayBot`;
-global.gt = 'YerayBot';
-global.mysticbot = 'YerayBot';
-global.mysticbot = 'https://github.com/Kimbrly1/';
-global.nomorown = '593992402778';
+global.wm2 = `${dia} ${fecha}\KimBot`;
+global.gt = 'KimBot';
+global.mysticbot = 'KimBot';
+global.mysticbot = 'https://github.com/Kimbrly1/KIMRBOT-MD';
+global.nomorown = '50433102145';
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf'];
 global.cmenut = '❖––––––『';
 global.cmenub = '┊✦ ';
