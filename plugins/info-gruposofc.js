c7297cc93a86ac999a9ddf89dfd9acc014a32502
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-30 18:23:28 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,7 +2,7 @@ let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '📇', key: m.key } })
-let str = `*🔰 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 🔰
+let str = `🔰 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 🔰
 
     *_╭━━━⊜ 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿 👽💥_*
   *_┃💎❏ ${gp1}_*
