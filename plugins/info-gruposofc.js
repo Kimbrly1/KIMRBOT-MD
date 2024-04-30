1084e752d0d3c84b69ae52846fcff87d47e3e70d
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-30 18:22:57 -0500

    Actualizar info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -2,26 +2,14 @@ let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     await conn.sendMessage(m.chat, { react: { text: '📇', key: m.key } })
-let str = `*🔮 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎*
-
-    *_╭━━━⊜ 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿_*
-  *_┃🌩❏ ${gp1}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*
-
-    *_╭━━━⊜ 𝘽 𝘖 𝘛 𝘚 :  𝙈 𝘝 𝘗 🔥🤖_*
-  *_┃🌩❏ ${gp2}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*
-
- 🔮 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎
+let str = `*🔰 𝙂𝙍𝙐𝙋𝙊𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 🔰
 
     *_╭━━━⊜ 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿 👽💥_*
   *_┃💎❏ ${gp1}_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
-  🔮 CANAL OFICIAL*
-
     *_╭━━━⊜ 𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩 - 𝘾𝙤𝙢𝙢𝙪𝙣𝙞𝙩𝙮 👽💥
-  *_┃💎❏ ${channel}_*
+  *_┃💎❏ ${gp4}_*
 *_╰━━━━━━━━━━━━━━━━⊜_*
 
 
