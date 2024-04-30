9a56097e7e558c46de74d8fcb15a008336e2ec5a
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-04-30 18:30:37 -0500

    Actualizar info-script.js

--- plugins/info-script.js
+++ plugins/info-script.js
@@ -8,7 +8,7 @@ const handler = async (m, { conn, args }) => {
       txt += `◦  *Visitantes:* ${json?.watchers_count || '-'}\n`;
       txt += `◦  *𝐓𝐚𝐦𝐚𝐧̃𝐨:* ${(json?.size / 1024).toFixed(2) || '-'} MB\n`;
       txt += `◦  *𝐀𝐜𝐭𝐮𝐚𝐥𝐢𝐳𝐚𝐜𝐢𝐨𝐧:* ${moment(json?.updated_at).format('DD/MM/YY - HH:mm:ss') || '-'}\n`;
-      txt += `◦  *Url:* ${json?.html_url || https://www.instagram.com/yeray_bot_md?igsh=MXRzMW43YWJ5cTF5Zw=='}\n\n`;
+      txt += `◦  *Url:* ${json?.html_url || https://.github.com/Kimbrly1/KIMRBOT-MD'}\n\n`;
       txt += `         ${json?.forks_count || '-'} Forks · ${json?.stargazers_count || '-'} Stars · ${json?.open_issues_count || '-'} Issues`;
    await conn.sendMessage(m.chat, {text: txt.trim(), mentions: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...txt.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `https://www.instagram.com/yeray_bot_md`, "sourceUrl": `https://www.instagram.com/yeray_bot_md`}}}, {quoted: m});
 };
