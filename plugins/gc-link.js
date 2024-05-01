cb7f70cc640393d26ad5d27cf83be6636180b5a6
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-01 01:11:03 -0500

    Actualizar gc-link.js

--- plugins/gc-link.js
+++ plugins/gc-link.js
@@ -4,7 +4,7 @@ const handler = async (m, {conn, args}) => {
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
        title: '𝐿𝑖𝑛𝑘 𝐺𝑟𝑜𝑢𝑝',
-      body: '𝙔𝙚𝙧𝙖𝙮𝘽𝙤𝙩-𝙈𝘿✅',    
+      body: '𝙆𝙞𝙢𝘽𝙤𝙩-𝙈𝘿❤️‍🩹',    
       previewType: 0, thumbnail: fs.readFileSync('./Menu.png'),
  sourceUrl: `https://atom.bio/sebas_official`}}});
 };     
