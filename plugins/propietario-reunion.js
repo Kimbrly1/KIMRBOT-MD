f7d3e6a6d18e559869c1c4f1fc5200224cebdf97
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-01 01:07:44 -0500

    Actualizar propietario-reunion.js

--- plugins/propietario-reunion.js
+++ plugins/propietario-reunion.js
@@ -5,10 +5,10 @@ let handler = async(m, { conn, command, text }) => {
   if (!text) return m.reply(`🚀 ¡𝗖𝘂𝗮𝗹 𝗲𝘀 𝗲𝗹 𝗠𝗼𝘁𝗶𝘃𝗼 𝗽𝗮𝗿𝗮 𝗹𝗮 𝗿𝗲𝘂𝗻𝗶𝗼𝗻 𝗱𝗲𝗹 𝘀𝘁𝗮𝗳𝗳!`)
     if (text.length < 10) return m.reply(`¡𝗠𝗼𝘁𝗶𝘃𝗼 𝗺𝘂𝘆 𝗰𝗼𝗿𝘁𝗼 , 𝗺𝗶𝗻𝗶𝗺𝗼 10 𝗰𝗮𝗿𝗮𝗰𝘁𝗲𝗿𝗲𝘀!`)
 
-    let texto = `🌸 @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: 
+    let texto = `📌 @${m.sender.split`@`[0]} 𝐀 𝐒𝐨𝐥𝐢𝐜𝐢𝐭𝐚𝐝𝐨 𝐋𝐚 𝐏𝐫𝐞𝐬𝐞𝐧𝐜𝐢𝐚 𝐃𝐞 𝐋𝐨𝐬 𝐎𝐰𝐧𝐞𝐫 𝐏𝐨𝐫 𝐌𝐨𝐭𝐢𝐯𝐨: 
 ¡${text}
-Staff Yeraybot - Sebastián🌹`
-m.reply('🌸 Se Le Ah Enviado El Mensaje Ah Los Propietarios Del Bot')
+Staff KimBot - Sebastián🔰`
+m.reply('📌 Se Le Ah Enviado El Mensaje Ah Los Propietarios Del Bot')
 for (let [jid] of global.owner.filter(([number, _, isDeveloper]) => isDeveloper && number)) {
                         let data = (await conn.onWhatsApp(jid))[0] || {}
                         if (data.exists)
