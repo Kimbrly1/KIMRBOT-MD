063040c4f47cc572664d760671de2030f762ba18
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-04 21:55:28 -0500

    Update autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen8, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝙆𝙞𝙢𝘽𝙤𝙩-𝙈𝘿❤️‍🩹', body: '𝖡𝗒: Kim - 𝖶𝖺𝗈𝗌', sourceUrl: `https://www.instagram.com/sebas.mvp_official0?igsh=cGk3Zmd0c3Bta280`, thumbnail: imagen6}}})}
+contextInfo: { externalAdReply: { title: '𝙆𝙞𝙢𝘽𝙤𝙩-𝙈𝘿❤️‍🩹', body: '𝖡𝗒: Kim - 𝖶𝖺𝗈𝗌', sourceUrl: `https://www.instagram.com/sebas.mvp_official0`, thumbnail: imagen6}}})}
 
 return !0 }
 export default handler
