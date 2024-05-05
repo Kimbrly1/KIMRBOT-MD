4e4a9784ac1e99221d5c14cf2d23e7c1b85cc068
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-04 21:54:08 -0500

    Update gc-kick2.js

--- plugins/gc-kick2.js
+++ plugins/gc-kick2.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, participants, usedPrefix, command}) => {
-  if (!global.db.data.settings[conn.user.jid].restrict) throw '*¡Este Comando Esta Desabilitado Por El Propietario Del Bot!*';
-  const kicktext = `🌻 𝐄𝐭𝐢𝐪𝐮𝐞𝐭𝐚 𝐀 𝐋𝐚 𝐏𝐞𝐫𝐬𝐨𝐧𝐚 𝐎 𝐑𝐞𝐬𝐩𝐨𝐧𝐝𝐚 𝐀𝐥 𝐌𝐞𝐧𝐬𝐚𝐣𝐞 𝐏𝐚𝐫𝐚 𝐄𝐥𝐢𝐦𝐢𝐧𝐚𝐫𝐥𝐨.\n\n*🦋 𝑬𝒋𝒆𝒎𝒑𝒍𝒐:*\n*${usedPrefix + command} @${global.suittag}*`;
+  if (!global.db.data.settings[conn.user.jid].restrict) throw '*𝙀𝙎𝙏𝙀 𝘾𝙊𝙈𝘼𝙉𝘿𝙊 𝙀𝙎𝙏𝘼 𝘿𝙀𝙎𝙃𝘼𝘽𝙄𝙇𝙄𝙏𝘼𝘿𝙊 𝙋𝙊𝙍 𝙀𝙇 𝙋𝙍𝙊𝙋𝙄𝙀𝙏𝘼𝙍𝙄𝙊 𝘿𝙀𝙇 𝘽𝙊𝙏*';
+  const kicktext = `🔰 *𝙀𝙏𝙄𝙌𝙐𝙀𝙏𝘼 𝘼 𝙇𝘼 𝙋𝙀𝙍𝙎𝙊𝙉𝘼 𝙊 𝙍𝙀𝙎𝙋𝙊𝙉𝘿𝘼 𝘼 𝙐𝙉 𝙈𝙀𝙉𝙎𝘼𝙅𝙀 𝙋𝘼𝙍𝘼 𝙀𝙇𝙄𝙈𝙄𝙉𝘼𝙍𝙇𝙊.*\n\n*𝙀𝙅𝙀𝙈𝙋𝙇𝙊:*\n*${usedPrefix + command} @${global.suittag}*`;
   if (!m.mentionedJid[0] && !m.quoted) return m.reply(kicktext, m.chat, {mentions: conn.parseMention(kicktext)});
   if (m.mentionedJid.includes(conn.user.jid)) return;
   const user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
