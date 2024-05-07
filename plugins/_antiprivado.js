a87b2ca305c92201de234995dd589b78c31a9bbd
sebasmpv <163625493+sebasmpv@users.noreply.github.com>
2024-05-07 12:35:45 -0500

    Actualizar _antiprivado.js

--- plugins/_antiprivado.js
+++ plugins/_antiprivado.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`👋 Hola Mi Estimado @${m.sender.split`@`[0]}, Perdón Pero No Esta Permitido Escribirme Al Privado Por Lo Cual Serás Bloqueado/A\n\n*Puedes Unirte A Mi Grupo Oficial* 😺\n\n${gp1}`, false, {mentions: [m.sender]});
+    await m.reply(`🔰 𝙃𝙊𝙇𝘼 𝙈𝙄 𝙀𝙎𝙏𝙄𝙈𝘼𝘿𝙊 🔰 @${m.sender.split`@`[0]}, 𝙇𝙊 𝙎𝙄𝙀𝙉𝙏𝙊 𝙋𝙀𝙍𝙊 𝙉𝙊 𝙀𝙎𝙏𝘼 𝙋𝙀𝙍𝙏𝙄𝙈𝙄𝘿𝙊 𝙀𝙎𝘾𝙍𝙄𝘽𝙄𝙍𝙈𝙀 𝘼𝙇 𝙋𝙍𝙄𝙑𝘼𝘿𝙊 \n\n𝘾𝙊𝙉𝙏𝘼𝘾𝙏𝘼 𝘾𝙊𝙉 𝙈𝙄 𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝙋𝘼𝙍𝘼 𝘿𝙀𝙎𝘽𝙇𝙊𝙌𝙐𝙀𝘼𝙍𝙏𝙀\n\n wa.me/593992402778`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
